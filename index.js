b8e513d672b5a88be6e368251dcc9ae8e3348bf7
LeiDeMing <136371773@qq.com>
2020-03-23 16:39:15 +0800

    add

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
-const Koa = require('koa')
-const router = require('./router')
-const app = new Koa()
+// const Koa = require('koa')
+// const router = require('./router')
+// const app = new Koa()
 
-app
-    .use(router.routes())
-    .use(router.allowedMethods())
+// app
+//     .use(logger)
+//     .use(router.routes())
+//     .use(router.allowedMethods())
 
-app.listen(1521, () => {
-    console.log('server start at 1521');
+// app.listen(1521, () => {
+//     console.log('server start at 1521');
+// })
+// const test = require('./config/test')
+const logger = require('./middlewares/log')
+const config = require('./config')
+const {
+    dbOpen
+} = require('./lib/db/connect')
+const read = require('./lib/db/read')
+const http = require('http')
+const createHandler = require('github-webhook-handler')
+const handler = createHandler({
+    path: config.path,
+    secret: config.readingSecret
 })
+
+dbOpen()
+
+http.createServer(function (req, res) {
+    handler(req, res, function (err) {
+        res.statusCode = 200
+        logger.info(`[info]：Success！`)
+        res.end('Success！')
+    })
+}).listen(1521)
+
+handler.on('error', function (err) {
+    logger.info(`[Error]：${err.message}`)
+})
+
+handler.on('push', function (event) {
+    logger.info(`[push]：${JSON.stringify(event.payload)}`)
+})
+
+handler.on('issues', async (event) => {
+    if (event.payload) {
+        const {
+            action,
+            issue
+        } = event.payload
+        if (action === 'opened') {
+            await read.addData(issue, event.payload)
+        }
+    }
+    logger.info(`[issues]：${JSON.stringify(event.payload)}`)
+})
+// read.addData(test.issue, test)
--- router.js
+++ router.js
@@ -1,8 +1,14 @@
 const router = require('koa-router')()
 
 
-router.post('/webhooks_reading', async (ctx, next) => {
-    console.log(ctx)
+router.get('/webhooks_reading', async (ctx, next) => {
+    // ctx.logger.info(`[request]：${JSON.stringify(ctx.req)}`)
+    // console.log(ctx.request.body)
+    ctx.status = 200
+    ctx.body = {
+        status: 0
+    }
+    await next();
 })
 
 module.exports = router
